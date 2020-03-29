71c134fc4406cb26e4818c24c73cdfe542ad10f2
Surendra Galwa <surendra.galwa@nymo.ai>
2020-03-29 18:07:40 +0530

    Added eye detection code

--- face_detection.py
+++ face_detection.py
@@ -1,7 +1,10 @@
 import cv2
 
-# Load the cascade
+# Load the Face detection cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Load Eye detection cascade
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # To capture video from webcam. 
 cap = cv2.VideoCapture(0)
@@ -19,6 +22,14 @@ while True:
         # Draw the rectangle around each face
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w] 
+            roi_color = img[y:y+h, x:x+w] 
+
+            # Detect the eyes
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 177, 255), 2)
+
         # Display
         cv2.imshow('Detected Face', img)
         # Stop if escape key is pressed
